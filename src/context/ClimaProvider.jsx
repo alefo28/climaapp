4d5c1644527f72d5c0fac3d919e36c4651e45147
alefo28 <alefo28@gmail.com>
2023-09-05 11:41:06 -0600

    first commit

---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+
+const ClimaContext = createContext();
+
+const ClimaProvider = ({ children }) => {
+  const [busqueda, setBusqueda] = useState({ ciudad: "", pais: "" });
+
+  const datosBusqueda = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+  const [noResultado, setNoResultado] = useState(false);
+
+  const consultarClima = async (datos) => {
+    setCargando(true);
+    setNoResultado(false);
+    try {
+      const { ciudad, pais } = datos;
+
+      const appId = import.meta.env.VITE_API_KEY;
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+
+      const { data } = await axios(url);
+      const { lat, lon } = data[0];
+
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+      const { data: clima } = await axios(urlClima);
+
+      setResultado(clima);
+    } catch (error) {
+      setResultado({});
+      setNoResultado("No hay resultados");
+    } finally {
+      setCargando(false);
+    }
+  };
+
+  return (
+    <ClimaContext.Provider
+      value={{
+        busqueda,
+        datosBusqueda,
+        consultarClima,
+        resultado,
+        cargando,
+        noResultado,
+      }}
+    >
+      {children}
+    </ClimaContext.Provider>
+  );
+};
+
+export { ClimaProvider };
+
+export default ClimaContext;
